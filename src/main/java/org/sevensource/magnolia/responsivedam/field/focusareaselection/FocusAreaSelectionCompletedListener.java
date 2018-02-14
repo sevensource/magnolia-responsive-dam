cd51a75696816028aa885ffc309d074e2007cf7b
pgaschuetz <philipp@sevensource.org>
2018-02-14 17:17:52 +0100

    * cleanup

--- src/main/java/org/sevensource/magnolia/responsivedam/field/focusareaselection/FocusAreaSelectionCompletedListener.java
+++ src/main/java/org/sevensource/magnolia/responsivedam/field/focusareaselection/FocusAreaSelectionCompletedListener.java
@@ -3,5 +3,5 @@ package org.sevensource.magnolia.responsivedam.field.focusareaselection;
 import org.sevensource.magnolia.responsivedam.focusarea.FocusAreas;
 
 public interface FocusAreaSelectionCompletedListener {
-	public void completed(boolean canceled, FocusAreas focusAreas);
+	void completed(boolean canceled, FocusAreas focusAreas);
 }
